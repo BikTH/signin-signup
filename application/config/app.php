c3eca1ff7cab09f1729113b5ed6481107ba9c7eb
BikTh <thierrybikoko8@gmail.com>
2022-08-22 13:37:04 +0100

    a

--- application/config/app.php
+++ application/config/app.php
@@ -1,11 +1,12 @@
 <?php
 
-$config["app"] = "Flamarya";
+$config["app"] = "signin";
 
 
 
 
-$config['base_url'] = 'https://oxt-log-reg.herokuapp.com/';
+//$config['base_url'] = 'https://oxt-log-reg.herokuapp.com/';
+$config['base_url'] = 'https://signin-signup.dev/';
 $config["default_timezone"] = "Africa/Douala";
 
 
--- application/views/website/home.php
+++ application/views/website/home.php
@@ -1,24 +1,37 @@
 <?php ?>
  
+
+<div class="container">
+    <div class="row justify-content-center g-3">
+        <!--<div class="col-md-10 offset-md-1">
+            <div class="row ">-->
+                <div class="col-md-5 offset-md-1 d-lg-block">
+                    <img src="<?= assets('img/imglogin.svg') ?>" class="img-fluid" alt="login img">
+                </div>
+                <div class="col-md-5">
+                    <div class="justify-content-between d-flex"><span class="fw-bold">Sign-in</span><span>or <a href="">create an acount</a></span></div>
+                </div>
+            <!--</div>
+        </div>-->
+    </div>
+</div>
+
+
 <div class="container py-4">
     <div class=" row justify-content-center ">
         <div class="card-body p-0">
             <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1 col-md-10 offset-md-1 ">
                 <div class="card bg-transparent text-sendary o-hidden border-0 my-5">
-                    <!--<h2 class="form-header text-primary text-center"> Welcome Back </h2>
-                    <div class=" text-center alert alert-danger alert-dismissible fade show " role="alert">
-                        a
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>-->
                     <div class="row justify-content-center d-flex align-items-center ">
-                        <!-- <div class="col-lg-6 d-lg-block p-5 text-center d-none "> -->
                         <div class="col-lg-6 d-lg-block p-5 text-center">
                             <img src="<?= assets('img/imglogin.svg') ?>" class="img-fluid" alt="login img">
                         </div>
                         <div class="col-lg-6 p-5 text-secondary">
                             <div class="justify-content-between d-flex"><span class="fw-bold">Sign-in</span><span>or <a href="">create an acount</a></span></div>
                             <div class="d-grid gap-2">
-                                <a href="" class="row btn btn-primary"><span class="fw-bold col-2 text-start"><i class="bi bi-google"></i></span><span class="fw-bold col-10 text-center">Sign in with google</span></a>
+                                <a href="#" class=" btn _black">
+                                    <span class="text-start"><i class="bi bi-google"></i></span><span class="fw-bold text-center">Sign in with google</span>
+                                </a>
                             </div>
                             <form action="" method="post" enctype="multipart/form-data"   class="row needs-validation" class="needs-validation" novalidate>
                                 <!-- <h2 class="form-header text-primary justify-content-left"> Welcome Back </h2> -->
